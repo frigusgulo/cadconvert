d768b28d66c0c3d29329ce7ed23bbe265d926ab3
Franklyn Dunbar <franklyn.dunbar@umontana.edu>
2021-04-23 16:27:52 -0600

    Cleaned Data

--- batchconvert.py
+++ batchconvert.py
@@ -1,9 +1,29 @@
 import os
 from cadconvert import convert 
 from glob import glob
+import shutil
+from pathlib import Path
+qcadpath = "/home/dunbar/Apps/qcad-3.26.2-pro-linux-x86_64/dwg2dwg"
 
-qcadpath = "/home/fdunbar/Applications/qcad-3.25.2-pro-linux-x86_64/dwg2dwg "
-dwgfiles = glob("/home/fdunbar/bullfrog/GEOPHY/DRAWINGS/*.DWG")
-strips = [os.path.splitext(file)[0] for file in dwgfiles]
-converters = [convert(dwgfiles,strips,qcadpath) for (dwgfiles,strips) in zip(dwgfiles,strips)]
-[converter.load() for converter in converters]
+root_dir = "/home/dunbar/Hillary/BarrickData"
+for root,dirs,files in os.walk(root_dir):
+	for directory in dirs:
+		newdir = directory.replace(" ","").replace("\t","")
+		if newdir is not directory:
+			shutil.move(os.path.join(root,directory),os.path.join(root,newdir))
+
+print(f"\n Cleaning Files \n")
+for root,dirs,files in os.walk(root_dir):
+	for file in files:
+		newfile = file.replace(" ","").replace("\t","")
+		if newfile is not file:
+			os.rename(os.path.join(root,file),os.path.join(root,newfile))
+			
+print("Gathering Cad Files")
+
+dwgfiles = [path for path in Path(root_dir).rglob("*.DWG")] 
+dwgfiles.extend([path for path in Path(root_dir).rglob("*.dwg")] )
+print("Converting Files")
+converters = [convert(dwgfiles,qcadpath) for dwgfiles in dwgfiles]
+print(len(converters))
+[converter.run() for converter in converters]
--- cadconvert.py
+++ cadconvert.py
@@ -2,50 +2,91 @@ import geopandas as gpd
 import os
 import pyproj
 from shapely.geometry import Point, LineString,Polygon
-
+import pdb
 class convert:
 	def __init__(self,
 		infile: str,
-		outfile: str,
 		qcadpath:str,
-		crs = "EPSG:32610"):
+		crs = "EPSG:32611"):
+
 		self.crs = crs
 		self.infile = infile
-		self.outfile = outfile
 		self.qcadpath = qcadpath
 		self.file = None
+		
+		self.filedir = os.path.join(os.path.splitext(self.infile)[0],"Processed_From_Here")
+		self.filedir_shape = os.path.join(self.filedir,"shapefiles")
+		self.filedir_cad = os.path.join(self.filedir,"cadfiles")
+
+		try:
+			os.makedirs(self.filedir)
+			os.makedirs(self.filedir_shape)
+			os.makedirs(self.filedir_cad)
+		except Exception as e:
+			pass
+			#print(f"Directory Construction Failed For {self.filedir}:\n {e}\n")
+
+    	
+		#pdb.set_trace()
 		self.reformat()
 
-
-
 	def reformat(self):
-		output = os.path.splitext(self.infile)[0]+".dxf"
-		os.system(self.qcadpath+ "-r=R27 " + "-outfile="+output +" "+self.infile)
+		output = str(os.path.join(self.filedir_cad,os.path.basename(os.path.splitext(self.infile)[0])+".dxf"))
+		if not os.path.isfile(output):
+			print(f"\n{self.infile} --> {output}\n")
+			os.system(self.qcadpath+ " -r=R27 " + "-outfile="+output +" "+str(self.infile))
 		self.file=output
 
 	def load(self):
 		mine = gpd.read_file(self.file)
-		mine = gpd.GeoDataFrame(mine)
-		mine.crs = self.crs
-		crs_utm = pyproj.CRS(self.crs)
-		mine.to_crs(crs_utm)
-		try:
-			self.dfpoints = mine[mine["geometry"].apply(lambda x: isinstance(x,Point)) ]
-			pointpath = self.outfile+"_points.shp"
-			self.dfpoints.to_file(pointpath,driver="ESRI Shapefile")
-		except:
-			print(f"\n Points Not Fond\n")
-		try:
-			self.dflines =  mine[mine["geometry"].apply(lambda x: isinstance(x,LineString)) ]
-			linepath = self.outfile+"_lines.shp"
-			self.dflines.to_file(linepath,driver="ESRI Shapefile")
-		except:
-			print(f"\n Lines Not Fond\n")
-		try:
-			self.dfpolygons = mine[mine["geometry"].apply(lambda x: isinstance(x,Polygon)) ]
-			polygonpath = self.outfile+"_polys.shp"
-			self.dfpolygons.to_file(polygonpath,driver="ESRI Shapefile")
+		self.mine = gpd.GeoDataFrame(mine)
+		self.mine.crs = self.crs
+		self.crs_utm = pyproj.CRS(self.crs)
+		self.mine.to_crs(self.crs_utm)
 
-		except:
-			print(f"\n Polygons Not Fond\n")
+	def check_exception(self):
+		if converter.mine["geometry"][0].bounds[0] < 100000:
+			print(f"Converting Geoseries\n")
+			self.mine["geometry"] = self.mine.translate(515960-9000,4084266-9000)
+
+
+	def to_file(self):
+
+		try:
+			self.dfpoints = self.mine[self.mine["geometry"].apply(lambda x: isinstance(x,Point)) ]
+			pointpath = os.path.basename(os.path.splitext(self.infile)[0])+"_points.shp"
+			pointpath = os.path.join(self.filedir_shape,pointpath)
+			if not os.path.isfile(pointpath):
+				self.dfpoints.to_file(pointpath,driver="ESRI Shapefile")
+		except Exception as e:
+			print(f"Point File Construction Failed For {self.infile}:\n {e}\n")
+
+
+		try:
+			self.dflines =  self.mine[self.mine["geometry"].apply(lambda x: isinstance(x,LineString)) ]
+			linepath = os.path.basename(os.path.splitext(self.infile)[0])+"_lines.shp"
+			linepath = os.path.join(self.filedir_shape,linepath)
+			if not os.path.isfile(linepath):
+				self.dflines.to_file(linepath,driver="ESRI Shapefile")
+		except Exception as e:
+			print(f"Line File Construction Failed For {self.infile}:\n {e}\n")
+
+
+		try:
+			self.dfpolygons = self.mine[self.mine["geometry"].apply(lambda x: isinstance(x,Polygon)) ]
+			polygonpath = os.path.basename(os.path.splitext(self.infile)[0])+"_polys.shp"
+			polygonpath = os.path.join(self.filedir_shape,polygonpath)
+			if not os.path.isfile(polygonpath):
+				self.dfpolygons.to_file(polygonpath,driver="ESRI Shapefile")
+
+		except Exception as e:
+			print(f"Polygon File Construction Failed For {self.infile}:\n {e}\n")
+
+	def run(self):
+		try:
+			self.load()
+			self.check_exception()
+			self.to_file()
+		except Exception as e:
+			print(f"Run Failed For {self.infile}:\n {e}\n")
 
