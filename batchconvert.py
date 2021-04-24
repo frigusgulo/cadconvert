ddbd4c30f14d68ec2fe5e50e15539d1cb852ba72
Franklyn Dunbar <franklyn.dunbar@umontana.edu>
2021-04-24 14:14:36 -0600

    update

--- batchconvert.py
+++ batchconvert.py
@@ -8,22 +8,36 @@ qcadpath = "/home/dunbar/Apps/qcad-3.26.2-pro-linux-x86_64/dwg2dwg"
 root_dir = "/home/dunbar/Hillary/BarrickData"
 for root,dirs,files in os.walk(root_dir):
 	for directory in dirs:
-		newdir = directory.replace(" ","").replace("\t","")
+		newdir = directory.replace(" ","").replace("\t","").replace("\'","").replace("-","_").replace(",","").replace("&","_and_").replace("(","").replace(")","")
 		if newdir is not directory:
 			shutil.move(os.path.join(root,directory),os.path.join(root,newdir))
 
 print(f"\n Cleaning Files \n")
 for root,dirs,files in os.walk(root_dir):
 	for file in files:
-		newfile = file.replace(" ","").replace("\t","")
+		newfile = file.replace(" ","").replace("\t","").replace("\'","").replace("-","_").replace(",","").replace("&","_and_").replace("(","").replace(")","")
 		if newfile is not file:
 			os.rename(os.path.join(root,file),os.path.join(root,newfile))
-			
-print("Gathering Cad Files")
+
+print("\nConverting WK1 Files\n")
+spreadfiles = [path for path in Path(root_dir).rglob(".WK?")]
+spreadfiles.extend([path for path in Path(root_dir).rglob(".xl??")])
+
+spreads = [convert(path,cadfile=False) for path in spreadfiles]
+
 
 dwgfiles = [path for path in Path(root_dir).rglob("*.DWG")] 
 dwgfiles.extend([path for path in Path(root_dir).rglob("*.dwg")] )
-print("Converting Files")
-converters = [convert(dwgfiles,qcadpath) for dwgfiles in dwgfiles]
-print(len(converters))
-[converter.run() for converter in converters]
+print("\nConverting Files\n")
+converters = [convert(dwgfile,qcadpath) for dwgfile in dwgfiles]
+total = len(converters)
+print(total)
+count = 0
+for converter in converters:
+	try:
+		converter.run()
+		count += 1
+	except Exception as e:
+		print(e)
+print(f"Successfull Processes: {(count/total)*100}%")
+
--- cadconvert.py
+++ cadconvert.py
@@ -6,7 +6,8 @@ import pdb
 class convert:
 	def __init__(self,
 		infile: str,
-		qcadpath:str,
+		qcadpath:str=None,
+		cadfile : bool = True,
 		crs = "EPSG:32611"):
 
 		self.crs = crs
@@ -14,28 +15,48 @@ class convert:
 		self.qcadpath = qcadpath
 		self.file = None
 		
-		self.filedir = os.path.join(os.path.splitext(self.infile)[0],"Processed_From_Here")
-		self.filedir_shape = os.path.join(self.filedir,"shapefiles")
-		self.filedir_cad = os.path.join(self.filedir,"cadfiles")
+		self.filedir = os.path.join(os.path.splitext(self.infile)[0],"_processed_")
+		if cadfile:
+			self.filedir_shape = os.path.join(self.filedir,"shapefiles")
+			self.filedir_cad = os.path.join(self.filedir,"cadfiles")
 
-		try:
-			os.makedirs(self.filedir)
-			os.makedirs(self.filedir_shape)
-			os.makedirs(self.filedir_cad)
-		except Exception as e:
-			pass
-			#print(f"Directory Construction Failed For {self.filedir}:\n {e}\n")
-
-    	
+			try:
+				os.makedirs(self.filedir)
+				os.makedirs(self.filedir_shape)
+				os.makedirs(self.filedir_cad)
+			except Exception as e:
+				pass
+				#print(f"Directory Construction Failed For {self.filedir}:\n {e}\n")
+			self.reformat_cad()
+		else:
+			self.filedir_spread = os.path.join(self.filedir,"spreadsheets")
+			try:
+				os.makedirs(self.filedir_spread)
+			except:
+				pass
+			self.reformat_spread(self)
 		#pdb.set_trace()
-		self.reformat()
+		
 
-	def reformat(self):
+	def reformat_cad(self):
 		output = str(os.path.join(self.filedir_cad,os.path.basename(os.path.splitext(self.infile)[0])+".dxf"))
 		if not os.path.isfile(output):
 			print(f"\n{self.infile} --> {output}\n")
 			os.system(self.qcadpath+ " -r=R27 " + "-outfile="+output +" "+str(self.infile))
 		self.file=output
+
+	def reformat_spread(self):
+		print("\n Reformat SpreadSheets")
+		output = os.path.join(self.filedir_spread,os.path.basename(os.path.splitext(self.infile)[0])+".ods")
+		if not os.path.isfile(output):
+			print(f"\n{self.infile} --> {output}\n")
+			orig,ext = os.path.splitext(self.infile)
+			orig = orig + "_original_"
+			origdest = orig+ext 
+			shutil.copyfileobj(self.infile,origdest)
+			os.system("libreoffice --headless --invisible --convert-to ods "+self.infile)
+			newfile = os.path.splitext(self.infile)[0]+".ods"
+			os.rename(newfile,output)
 
 	def load(self):
 		mine = gpd.read_file(self.file)
@@ -45,21 +66,23 @@ class convert:
 		self.mine.to_crs(self.crs_utm)
 
 	def check_exception(self):
-		if converter.mine["geometry"][0].bounds[0] < 100000:
+		if self.mine["geometry"][0].bounds[0] < 100000:
 			print(f"Converting Geoseries\n")
 			self.mine["geometry"] = self.mine.translate(515960-9000,4084266-9000)
 
 
 	def to_file(self):
-
+		toshape = False
 		try:
 			self.dfpoints = self.mine[self.mine["geometry"].apply(lambda x: isinstance(x,Point)) ]
 			pointpath = os.path.basename(os.path.splitext(self.infile)[0])+"_points.shp"
 			pointpath = os.path.join(self.filedir_shape,pointpath)
 			if not os.path.isfile(pointpath):
 				self.dfpoints.to_file(pointpath,driver="ESRI Shapefile")
+				toshape=True
 		except Exception as e:
-			print(f"Point File Construction Failed For {self.infile}:\n {e}\n")
+			pass
+			#print(f"Point File Construction Failed For {self.infile}:\n {e}\n")
 
 
 		try:
@@ -68,8 +91,10 @@ class convert:
 			linepath = os.path.join(self.filedir_shape,linepath)
 			if not os.path.isfile(linepath):
 				self.dflines.to_file(linepath,driver="ESRI Shapefile")
+				toshape=True
 		except Exception as e:
-			print(f"Line File Construction Failed For {self.infile}:\n {e}\n")
+			pass
+			#print(f"Line File Construction Failed For {self.infile}:\n {e}\n")
 
 
 		try:
@@ -78,15 +103,17 @@ class convert:
 			polygonpath = os.path.join(self.filedir_shape,polygonpath)
 			if not os.path.isfile(polygonpath):
 				self.dfpolygons.to_file(polygonpath,driver="ESRI Shapefile")
+				toshape=True
 
 		except Exception as e:
-			print(f"Polygon File Construction Failed For {self.infile}:\n {e}\n")
+			pass
+			#print(f"Polygon File Construction Failed For {self.infile}:\n {e}\n")
 
+		if toshape:
+			print(f"Shapefiles processed for {self.infile}\n")
 	def run(self):
-		try:
-			self.load()
-			self.check_exception()
-			self.to_file()
-		except Exception as e:
-			print(f"Run Failed For {self.infile}:\n {e}\n")
-
+	
+		self.load()
+		self.check_exception()
+		self.to_file()
+	
